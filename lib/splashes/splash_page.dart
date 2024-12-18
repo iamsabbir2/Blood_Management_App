7ba37e72dd3d5dc9bb173ffbd2dec228f4dfa188
iamsabbir1 <ce21001@mbstu.ac.bd>
2024-12-18 21:42:46 +0600

    09:42pm - 18-12-2024

--- lib/splashes/splash_page.dart
+++ lib/splashes/splash_page.dart
@@ -6,7 +6,10 @@ import 'package:logger/logger.dart';
 
 class SplashPage extends StatefulWidget {
   final VoidCallback onInitializationComplete;
-  const SplashPage({super.key, required this.onInitializationComplete});
+  const SplashPage({
+    super.key,
+    required this.onInitializationComplete,
+  });
   @override
   State<SplashPage> createState() => _SplashPageState();
 }
